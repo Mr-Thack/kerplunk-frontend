b317ba2d0ada7077b9754319f01b8e966e1c4505
Abdul Muqeet Mohammed <mr_thack@yahoo.com>
2022-10-23 20:35:55 -0500

    Fixed Compilation Problems

--- svelte.config.js
+++ svelte.config.js
@@ -5,16 +5,21 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess({
+	preprocess: [preprocess({
+		postcss: true,
 		replace: [
 			[/WROOT/g,process.env.NODE_ENV==='production'?'https://24.99.231.214':'http://10.0.0.2:5173'],
 			[/CMPNTS/g,process.cwd() + '/src/components/'],
 			[/LIB/g,process.cwd() + '/src/lib/']
 		]
-	}),
+	})],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 	}
 };
 
