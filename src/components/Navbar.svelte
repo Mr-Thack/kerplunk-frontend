e3b531a97ad24d5f6102972f7db8f92128ae5725
Abdul Muqeet Mohammed <mr_thack@yahoo.com>
2022-12-03 15:32:35 -0500

    added chat, logo, button color

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,11 +1,11 @@
 <nav>
-  <a href="#">
-    <img src="/icon.jpg" />
-    <span style="font-size: 2em;">¡KerPlunk!<span>
+  <a href="/" style="margin-left:0.5em">
+    <img src="/icon.jpg" alt="icon" style="width:auto;height:auto;max-height:50%" />
+    <span style="font-size:2em">¡KerPlunk!<span>
   </a>
   <div class="menu">
-    <a href="/chat" class="psuedo button">Chat!</a>
-		<a href="/login" class="success button">Login</a>
-		<a href="/signup" class="button">Sign Up</a>
+    <a href="/chat" id='chat-b' class="button">Chat!</a>
+		<a href="/login" id='login-b' class="button">Login</a>
+		<a href="/signup" id='signup-b' class="button">Sign Up</a>
   </div>
 </nav>
