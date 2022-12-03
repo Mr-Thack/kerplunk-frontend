9593ae671fc990a309e4bca3804c4cd6d8c8ff4f
Abdul Muqeet Mohammed <mr_thack@yahoo.com>
2022-12-03 15:32:54 -0500

    added chat, logo, button color

---
+++ src/routes/chat/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+let messages = [{text:'hello',from:'user1',time:'2:39'},{text:'hi!',from:'user2',time:'2:40'}];
+</script>
+<chatbox>
+	<ul id='chat'>
+	{#each messages as msg}
+		<li><p>{msg.text}</p></li>
+	{/each}
+	</ul>
+</chatbox>
