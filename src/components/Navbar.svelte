ebb90d380b767bbd3946cc6f5a7c4ea489d17422
Abdul Muqeet Mohammed <mr_thack@yahoo.com>
2023-05-29 22:58:09 -0400

    App Rail added

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,19 +1,38 @@
 <script lang='ts'>
-  import '@skeletonlabs/skeleton/themes/theme-crimson.css';
-	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	import '../app.postcss';
-	import { LightSwitch, SlideToggle } from '@skeletonlabs/skeleton';
+  import { page } from '$app/stores';
+	import { AppRail, AppRailAnchor, LightSwitch, SlideToggle } from '@skeletonlabs/skeleton';
   let navs = [['chatrooms','Chat!'],['login','Login!']];
 </script>
-<nav>
-  <a href="/" style="margin-left:0.5em">
+  <!--
+  <a href="/" style="margin-left: 0.5em">
     <img src="/icon.jpg" alt="icon" style="width:auto;height:auto;max-height:50%" />
     <span style="font-size:2em">¡KerPlunk!<span>
   </a>
   <div class="lightswitch"><LightSwitch /></div>
   <div class="menu">
+    <div class="lightswitch">
+      <LightSwitch />
+    </div>
     {#each navs as n}
       <a href="/{n[0]}" id='{n[0]}-b' class="button">{n[1]}</a>
     {/each}
   </div>
-</nav>
+  -->
+  <AppRail>
+    <svelte:fragment slot="lead">
+      <AppRailAnchor href="/">
+        <img src="/icon.jpg" alt="icon" style="width:auto;height:auto;max-height:50%" />
+      </AppRailAnchor>
+    </svelte:fragment>
+    
+    {#each navs as n}
+      <AppRailAnchor href={n[0]} selected={$page.url.pathname === '/' + n[0]}>
+        <svelte:fragment slot="lead">(ICON)</svelte:fragment>
+        <span>{n[1]}</span>
+      </AppRailAnchor>
+    {/each}
+
+    <div class="lightswitch">
+      <LightSwitch />
+    </div>
+  </AppRail>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppShell } from '@skeletonlabs/skeleton';
 </script>
 
 <Navbar />
